d4239cadb9c362b1a5add79357f67cac8427ce3d
Rafael <83823157+rafaeucastro@users.noreply.github.com>
2024-08-09 16:06:02 -0300

    Commit Inicial

---
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -0,0 +1,31 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.usuario.DadosAutenticacao;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJWT;
+import med.voll.api.infra.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("login")
+public class AutenticacaoController {
+
+    @Autowired
+   private TokenService tokenService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping
+    public ResponseEntity<DadosTokenJWT> efetuarLogin(@Valid @RequestBody DadosAutenticacao dadosAutenticacao){
+        var authToken = new UsernamePasswordAuthenticationToken(dadosAutenticacao.login(), dadosAutenticacao.senha());
+        var authentication = authenticationManager.authenticate(authToken);
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+    }
+}
---
+++ src/main/resources/db/migration/V5__create-table-usuarios.sql
@@ -0,0 +1,7 @@
+create table Usuarios(
+    id bigint not null auto_increment,
+    login varchar(100) not null unique,
+    senha varchar(255) not null,
+
+    primary key(id)
+);
